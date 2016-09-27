2343825fdd58aae18d9d111cbf1df018637f8028
drishtiawasthi <drishti.awasthi@gmail.com>
2016-09-27 20:12:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,11 @@ var element = document.getElementById('main-text'
 );
 
 element.innerHTML ="New value";
+
+//Move the image
+var img = document.getElementById('img');
+img.onclick = function () {
+  img.style.marginLeft = '100px';  
+};
+
+
