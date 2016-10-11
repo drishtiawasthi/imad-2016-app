c234275e97421f022e6d8597bf43b7940fe89bd4
drishtiawasthi <drishti.awasthi@gmail.com>
2016-10-11 23:17:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,8 @@
 var button = document.getElementById('counter');
 button.onclick = function () {
     
+    
+
     //Create a request object
     var request = new XMLHttpRequest();
     
@@ -10,33 +12,31 @@ button.onclick = function () {
         if(request.readyState === XMLHttpRequest.DONE) {
             //Take some action
             if(request.status === 200) {
-                var counter = request.responseText;
-                  var span = document.getElementById('count');
-                  span.InnerHTML = counter.toString();
+                //Capture a list of names and render it as a list
+                var names = request.responseText;
+                names= JSON.parse(names);
+                var names =['name1', 'name2', 'name3', 'name4'];
+                var list = '';
+                for (var i=0; i<names.length; i++) {
+                    list += '<li>' + name[i] + '<li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+               
             }
         }
         //Not done yet
     };
     //Make the request
-    request.open('GET', 'http://http://drishtiawasthi.imad.hasura.io/counter', true);
+    request.open('GET', 'http://http://drishtiawasthi.imad.hasura.io/submit-name?name' + name, true);
     request.send(null);
 };
-
-
 
 //Submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit =document.getElementById('submit_btn');
 submit.onclick = function () {
-  //Make a request to the server and send the name
-  
-  //Capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list = '';
-    for (var i=0; i<names.length; i++) {
-        list += '<li>' + names[i] + '<li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+ 
 };
+    
